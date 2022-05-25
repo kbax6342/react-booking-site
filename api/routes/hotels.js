80c6e13f7609a34806432a6f18f1ea003f55f694
Kevin Nelms Baxter II <k3vin.baxt3r@gmail.com>
2022-05-24 20:19:52 -0400

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,23 @@
+import express from "express"
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, updateHotel, getHotel, getHotels } from "../controllers/hotel.js";
+
+const router = express.Router()
+
+//CREATE
+router.post("/", createHotel)
+
+//UPDATE
+router.put("/:id", updateHotel)
+
+//DELETE
+router.delete("/:id", deleteHotel)
+
+//GET
+router.get("/:id", getHotel)
+
+//GET ALL
+router.get("/", getHotels )
+
+export default router
